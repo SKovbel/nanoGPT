1e431e7fd7d072a6da9026d417641a01c798ce82
KSM <ksm.emails@gmail.com>
2025-05-23 12:21:04 +0200

    Update train.py

--- train.py
+++ train.py
@@ -29,7 +29,7 @@ from torch.distributed import init_process_group, destroy_process_group
 
 from model import GPTConfig, GPT
 
-torch.set_default_dtype(torch.float16)
+#torch.set_default_dtype(torch.float16)
 
 # -----------------------------------------------------------------------------
 # default config values designed to train a gpt2 (124M) on OpenWebText
